77ca8f607c2b765435bfd56aebed47b4b299c0e6
enderman19980125 <enderman19980125@outlook.com>
2021-06-29 14:34:09 +0800

    Import css of "bootstrap" and "bootstrap-vue".

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Brief/>
+    <b-navbar type="light" variant="light" sticky>
+      hello hello hello hello hello hello hello hello
+    </b-navbar>
+    <div style="height: 2000px;">
+      <div class="bg-danger">1</div>
+      <div class="bg-success">2</div>
+      <div class="bg-primary">3</div>
+    </div>
   </div>
 </template>
 
 <script>
-import Brief from "./components/personal_introduction/Brief.vue";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./../node_modules/bootstrap-vue/dist/bootstrap-vue.min.css";
 
 export default {
   name: 'App',
-  components: {
-    Brief,
-  }
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
